6f1be34064adcde4cc7ee03dca463962bd061e0b
bsiguenz <55162651+bsiguenz@users.noreply.github.com>
2019-11-10 18:49:28 -0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,7 +5,7 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-            'Bearer PSpOOTxpFCOosFWfG71kl1DJ4TgVk4LF-SbPuvSQoxy27X10ZVt69l3ualpn9WndB2vyJTB_m4P-nrYvNPvjV_oUBMA1doPuhluk0vybcexTzS-lr7eUMptSe3vEXXYx'
+            'Bearer <KEY GOES HERE>'
     }
 });
 
