931d12b49ef22b956f9616d71be4bcfc056c2be9
Bryan Siguenza <LionsDen@snay.fios-router.home>
2019-11-18 01:27:43 -0500

    input grocery, weight, tracker post ready need parameters from backend

---
+++ UI_v_03/src/screens/PriceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const PriceScreen = () => {
+    return(
+        <Text>PriceScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default PriceScreen;
