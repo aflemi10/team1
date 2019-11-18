7a034f9e7892a151ca33e0ea290694f72806b0da
Bryan Siguenza <LionsDen@snay.fios-router.home>
2019-11-18 13:33:48 -0500

    Item add, weight add, and carlies add POST request commpleted

---
+++ UI_v_04/src/components/ItemsDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+import ResultsList from './ResultsList';
+
+const ItemsDetail =({item}) =>{
+    return <View>
+        <Text style={styles.name}>
+            {item.name}
+        </Text>
+       
+    </View>
+};
+
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        borderRadius: 4,
+        height: 120,
+    },
+    name: {
+        fontWeight: 'bold',
+    }
+});
+
+export default ItemsDetail;
